f3976ebf14456547b13c3466b9c60319dba996c0
Avaamo <avaamo@Avaamos-MacBook-Pro.local>
2023-07-18 15:34:56 +0530

    till succesful signups but code aint workin

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   get '/signup', to:'users#new'
+  get '/show'  ,to: 'users#show'
   root 'main#home'
   get '/home', to:'main#home'
   get '/help', to: 'main#help'
   get '/login', to:'main#login'
+  resources :users
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
