fd98d495f39a51eae6c32ad3cf828809862cc5e0
Avaamo <avaamo@Avaamos-MacBook-Pro.local>
2023-07-18 11:12:36 +0530

    user name validation

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,15 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  def setup
+    @user=User.new(name: "Example User", email:user@example.com)
+  end
+  test "should be valid" do
+    aseert @user.valid?
+  end
+  test "name should be present" do
+    @user.name=" "
+    assert_not @user.valid?
+  end
+  
 end
