44b0c9e3e56a1a60d6a3ed49d2edf4b02b25bba3
Avaamo <avaamo@Avaamos-MacBook-Pro.local>
2023-07-21 01:00:07 +0530

    assignment done logout not working, for future can add password resets, follow and like

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+    has_many :microposts, dependent: :destroy
     before_save { self.email = email.downcase }
     validates :name, presence: true, length: {maximum: 50}
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -10,5 +11,8 @@ class User < ApplicationRecord
         BCrypt::Engine.cost
         
         BCrypt::Password.create(string, cost: cost)
+    end
+    def feed 
+        Micropost.where("user_id = ?", id)
     end
 end
